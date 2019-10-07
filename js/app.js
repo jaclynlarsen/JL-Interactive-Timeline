ccd3eac145d61aa304a893ea14e7afa85035152a
Jaclyn Larsen <jaclarsen9@aol.com>
2019-10-06 20:08:02 -0400

    Horizontal Scroll

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
 $(document).foundation()
+
+/* main variables */
+var panel, num, innerContent, $wrapper = $('.wrapper');
+
+/* wrapper width */
+function init(){
+  panel = $(window).width();
+  num = $('.panel').length;
+  innerContent = panel*num;
+  $wrapper.width(innerContent);
+}
+
+/* set wrapper width */
+init();
+
+/* if screen is resized, recalculate wrapper and panel width */
+$(window).resize(function(){
+  init();
+  // send scroller back to the start
+  $wrapper.css('transform','translate(0,0)');
+});
+
+// handle clicks
+$('li').click(function(){ 
+  var curr = $(this).index();
+  scroll(curr);
+});
+
+// handle scrolling
+function scroll(curr){
+  $wrapper.css('transform','translate('+(panel*curr)*-1+'px,0)');
+}
